fcd316c6a2e30233d3670b827a81b023e8be7c65
chingyen <ChingYenYang83@gmail.com>
2023-12-24 12:45:04 +0800

    similar_pair

--- cos_similarity_test/classify.py
+++ cos_similarity_test/classify.py
@@ -6,7 +6,8 @@ import tensorflow as tf
 import tensorflow_hub as hub
 from operator import itemgetter
 
-OUTPUT_PATH = "similar_prompt.json"
+OUTPUT_PATH = "similar_pair.json"
+THRESH = 0.7
 
 class USE:
     def __init__(self):
@@ -23,17 +24,20 @@ class USE:
 csv_file_path = 'harmful_behaviors.csv'
 
 # Initialize an empty list to store the values
+goal_list = []
 target_list = []
+
 
 # Read the CSV file and append the values to the list
 with open(csv_file_path, 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
-        target_list.append(row['goal'])
+        goal_list.append(row['goal'])
+        target_list.append(row['target'])
 
 use = USE()
 score_list = []
-max_len = len(target_list)
+max_len = len(goal_list)
 print(f"max_len = {max_len}")
 
 for i in range(max_len):
@@ -43,45 +47,57 @@ for i in range(max_len):
         if (i == j):
             sim_score = 0
         else:
-            sim_score = use.compute_sim([target_list[i]], [target_list[j]])
+            sim_score = use.compute_sim([goal_list[i]], [goal_list[j]])
         compare_list.append({"id": j, "score": sim_score})
     compare_list = sorted(compare_list, key=itemgetter('score'),reverse=True)
-    score_list.append(compare_list[:9])
+    score_list.append(compare_list[:40])
     
-avg_score = []
+# avg_score = []
 
-for i in range(max_len):
-    sum = 0
-    for j in range(9):
-        sum += score_list[i][j]['score']
-    print(sum/9)
-    avg_score.append({"id":i,"avg":sum/9})
-avg_score = sorted(avg_score, key=itemgetter('avg'), reverse=True)
-print(f"avg======================\n{avg_score[:9]}")
-best_id = [avg_score[i]['id'] for i in range(max_len)]
-print(f"best=======================\n{best_id[:9]}")
+# for i in range(max_len):
+#     sum = 0
+#     for j in range(9):
+#         sum += score_list[i][j]['score']
+#     print(sum/9)
+#     avg_score.append({"id":i,"avg":sum/9})
+# avg_score = sorted(avg_score, key=itemgetter('avg'), reverse=True)
+# print(f"avg======================\n{avg_score[:9]}")
+# best_id = [avg_score[i]['id'] for i in range(max_len)]
+# print(f"best=======================\n{best_id[:9]}")
 
 count = 0
 used = [False] * max_len
 output_list = []
-for i in range(max_len):
-    id = best_id[i]
-    any_used = False
-    if used[id]:
-        any_used = True
-    for j in range(9):
-        if (used[score_list[id][j]['id']]):
-            any_used = True
-            break
-    if not any_used:
-        output_list.append({"id": count, 'input': target_list[id]})
-        count += 1
-        used[id] = True
-        for k in range(9):
-            t_id = score_list[id][k]['id']
-            output_list.append({"id": count, 'input': target_list[t_id]})
-            count += 1
-            used[t_id] = True
+for id in range(max_len):
+    if not used[id]:
+       for i in range(40):
+            s_id = score_list[id][i]["id"]
+            if not used[s_id]:
+                if score_list[id][i]['score'] >= THRESH:
+                    used[s_id] = True
+                else:
+                    used[s_id] = True
+                    used[id] = True
+                    pair = {
+                        "id": count,
+                        "goal": goal_list[id],
+                        "target": target_list[id],
+                        "similar_goal": goal_list[s_id],
+                        "similar_target": target_list[s_id]
+                    }
+                    output_list.append(pair)
+                    count += 1
+                    break
+                          
+    # if not any_used:
+    #     output_list.append({"id": count, 'input': goal_list[id]})
+    #     count += 1
+    #     used[id] = True
+    #     for k in range(9):
+    #         t_id = score_list[id][k]['id']
+    #         output_list.append({"id": count, 'input': goal_list[t_id]})
+    #         count += 1
+    #         used[t_id] = True
     
 with open(OUTPUT_PATH, 'w', encoding='utf-8') as output_file:
     json.dump(output_list, output_file, ensure_ascii=False, indent=2)
